d9a0b359498fe5784f68d0be63e1390aa59e1c6d
Karen Nakahara <karenakahara@gmail.com>
2021-11-18 17:40:40 -0300

    criacao rotas

---
+++ source/App/Web.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Source\App;
+
+use Source\Core\Controller;
+
+class Web extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct(__DIR__ . "/../../themes/" . CONF_VIEW_THEME . "/");
+    }
+
+    public function home(): void
+    {
+        echo $this->view->render("home", [
+            "title"=> "CaféControl - Gerencie suas contas com o melhor café"
+        ]);
+    }
+
+    public function error(array $data): void
+    {
+        echo $this->view->render("error", [
+            "title"=> "{$data['errcode']} | Ops!"
+        ]);
+    }
+}
